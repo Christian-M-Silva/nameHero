658919a16659ba445849ea2734a5cdf2ddaf2c7a
Christian-M-Silva <tininjacms@gmail.com>
2022-11-10 07:14:42 -0300

    init repository e end project

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,100 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+const User = use('App/Models/User')
+
+/**
+ * Resourceful controller for interacting with users
+ */
+class UserController {
+  /**
+   * Show a list of all users.
+   * GET users
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index({ request, response, view }) {
+    const userData = await User.all()
+   
+    return userData
+  }
+
+  /**
+   * Create/save a new user.
+   * POST users
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store({ request, response }) {
+    const userData = request.only(['identidade', 'superHero'])
+
+    const user = await User.create(userData)
+
+    return user
+  }
+
+  /**
+   * Display a single user.
+   * GET users/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show({ params, request, response, view }) {
+    const dataUser = await User.findByOrFail('_id', params.id)
+    
+    return dataUser
+  }
+
+  /**
+   * Update user details.
+   * PUT or PATCH users/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update({ params, request, response }) {
+    const user = request.only(['identidade', 'superHero'])
+
+    const userData = await User.findByOrFail('_id', params.id)
+
+    await userData.merge({
+      identidade: user.identidade,
+      superHero: user.superHero,
+    })
+
+    await userData.save()
+
+    return userData
+
+  }
+
+  /**
+   * Delete a user with id.
+   * DELETE users/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy({ params, request, response }) {
+    const dataUser = await User.findByOrFail('_id', params.id)
+
+    await dataUser.delete()
+
+    return await User.all()
+  }
+}
+
+module.exports = UserController
